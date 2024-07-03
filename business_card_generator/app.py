376e6d4a402d361d0993842ba9a9e21a2e1b8bca
Nabeel Prior <nabeelp@users.noreply.github.com>
2024-07-03 14:52:35 +0000

    remove path debugging

--- business_card_generator/app.py
+++ business_card_generator/app.py
@@ -1,11 +1,3 @@
-import os
-
-# get the current working directory
-current_working_directory = os.getcwd()
-
-# print output to the console
-print(current_working_directory)
-
 import mimetypes
 
 from http import HTTPStatus
