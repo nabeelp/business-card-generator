becf1429f6618afe6f37e36a42ae839a877cd9cb
Nabeel Prior <nabeelp@users.noreply.github.com>
2024-07-03 13:48:56 +0000

    print current working directory for debug purposes

--- business_card_generator/app.py
+++ business_card_generator/app.py
@@ -1,3 +1,11 @@
+import os
+
+# get the current working directory
+current_working_directory = os.getcwd()
+
+# print output to the console
+print(current_working_directory)
+
 import mimetypes
 
 from http import HTTPStatus
